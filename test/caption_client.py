fbdefb4053145e753eb993af502b2be443764fe2
zuo wang <w.zuo@hotmail.com>
2019-08-14 14:31:04 -0400

    add reload option and time out for gunicorn

--- test/caption_client.py
+++ test/caption_client.py
@@ -1,9 +1,10 @@
 import requests
 if __name__ == '__main__':
     local_url = ' http://127.0.0.1:8000/predict'
+    gce_url = 'http://146.148.103.174:80/predict'
     # files = {'picture':open(input('image path: '),'rb'),'beam_size':input('beam size: ')}
     files = {'picture': open(input('image path: '), 'rb')}
-    r = requests.post(local_url,params = {'beam_size':input('beam size: ')},
+    r = requests.post(gce_url,params = {'beam_size':input('beam size: ')},
                       files=files,timeout=400)
 
     print(r.text)
