9a75b6fe38740c9d3daa42a7d464feab9182bd61
zuo wang <w.zuo@hotmail.com>
2019-08-14 17:12:59 -0400

    test bench update

--- test/caption_client.py
+++ test/caption_client.py
@@ -1,10 +1,12 @@
 import requests
 if __name__ == '__main__':
-    local_url = ' http://127.0.0.1:8000/predict'
-    gce_url = 'http://146.148.103.174:80/predict'
+    local_url = ' http://127.0.0.1:80/predict'
+    gc_caption_url = 'http://35.247.123.253:80/predict'
+    gce_translate_url = 'http://104.198.205.153:80/translate'
     # files = {'picture':open(input('image path: '),'rb'),'beam_size':input('beam size: ')}
-    files = {'picture': open(input('image path: '), 'rb')}
-    r = requests.post(gce_url,params = {'beam_size':input('beam size: ')},
+    online_img = '~/Caption/test/IMG_4039.jpg'
+    files = {'picture': open(online_img, 'rb')}
+    r = requests.post(gc_caption_url,params = {'beam_size':5,'translate_api':gce_translate_url},
                       files=files,timeout=400)
 
     print(r.text)
