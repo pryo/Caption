cc1f58dbb1db90e690ea2a7e94502e46e1dafe96
zuo wang <w.zuo@hotmail.com>
2019-08-14 13:40:23 -0400

    updated download script

--- model/download_model.py
+++ model/download_model.py
@@ -6,12 +6,14 @@ bucket_name='learnalbe-proj-model'
 # client = storage.Client()
 # bucket = client.get_bucket(config.MODEL_BUCKET)
 # blob = bucket.blob(config.)
+
 def download_blob(bucket_name, source_blob_name, destination_file_name):
     """Downloads a blob from the bucket."""
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(source_blob_name)
-
+    #blob = bucket.blob(source_blob_name)
+    blob = bucket.get_blob(source_blob_name)
+    #blob.download_to_filename(filename)
     blob.download_to_filename(destination_file_name)
 
     print('Blob {} downloaded to {}.'.format(
