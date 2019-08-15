74d5efff2bae66f7436cdbc45d8c7dbac8a901b4
zuo wang <w.zuo@hotmail.com>
2019-08-15 12:23:19 -0400

    updated the automation scrrpi to make it install torch 0.4.1, see what happens, downgraded scipy also

--- test/caption_client.py
+++ test/caption_client.py
@@ -3,11 +3,14 @@ if __name__ == '__main__':
     local_url = ' http://127.0.0.1:80/predict'
     gc_caption_url = 'http://146.148.103.174/predict'
     gce_translate_url = 'http://104.197.123.37/translate'
+    combine_url = 'http://146.148.103.174/caption_translate'
     # files = {'picture':open(input('image path: '),'rb'),'beam_size':input('beam size: ')}
     online_img = '~/Caption/test/IMG_4039.jpg'
     local_img = '/home/david/Developer/a-PyTorch-Tutorial-to-Image-Captioning/test/IMG_4039.jpg'
     files = {'picture': open(local_img, 'rb')}
-    r = requests.post(gc_caption_url,params = {'beam_size':5,'translate_api':gce_translate_url},
-                      files=files,timeout=400)
+    # r = requests.post(gc_caption_url,params = {'beam_size':5,'translate_api':gce_translate_url},
+    #                   files=files,timeout=400)
+    r = requests.post(combine_url, params={'beam_size': 5},
+                      files=files, timeout=400)
 
     print(r.text)
